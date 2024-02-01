ee2e746605b5d0e6d3f5d195c1725be5630b97ba
IAFEnvoy <tanksheng545@126.com>
2024-02-01 23:47:37 +0800

    change rule

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -27,28 +27,26 @@ class News extends React.Component {
           <header>
             <h1><Text>赛季规则</Text></h1>
           </header>
-          <h1><Text>测试赛季S1赛季规则</Text></h1>
-          <p><Text>允许使用的道具：</Text>
+          <h1><Text>S1赛季规则</Text></h1>
+          <p>
+            <Text>允许使用的物品</Text><br></br>
+            <Text>✅ 梯子</Text><br></br>
+            <Text>✅ 水只能在自己基地使用</Text><br></br>
+            <Text>✅ 跳跃、隐身药水在 绿宝石2 后可使用</Text><br></br>
+            <Text>✅ 搭路蛋在 钻石2 后可使用</Text><br></br>
+            <Text>✅ 允许使用火球 炸毁 对方钻点钻石</Text><br></br>
+            <Text>✅ Side侧岛 只能在Invasion和Lectus地图</Text><br></br>
           </p><p>
-            <Text>🟩 梯子</Text><br></br>
-            <Text>🟩 仅蓝队侧岛</Text><br></br>
-            <Text>🟩 钻甲钻剑</Text><br></br>
-            <Text>🟩 水桶(仅在自己基地使用)</Text><br></br>
-            <Text>🟩 TNT</Text><br></br>
-          </p><p>
-            <Text>床爆后允许使用的道具:</Text><br></br>
-            <Text>🟨 隐身药水</Text><br></br>
-            <Text>🟨 跳跃药水，速度药水</Text><br></br>
-            <Text>🟨 搭桥蛋</Text><br></br>
-            <Text>🟨 黄队基地(床爆后所有图side都允许)</Text><br></br>
+            <Text>床爆后允许使用的物品</Text><br></br>
+            <Text>🟨 黄队侧岛</Text><br></br>
             <Text>🟨 珍珠</Text><br></br>
+            <Text>🟨 速度药水、钻剑、钻甲</Text><br></br>
           </p><p>
-            <Text>不允许使用的道具:</Text><br></br>
-            <Text>🟥 禁止使用火球炸毁敌方钻石岛钻石</Text><br></br>
+            <Text>禁止使用的物品</Text><br></br>
             <Text>🟥 击退棒</Text><br></br>
-            <Text>🟥 弓（任何弓）</Text><br></br>
+            <Text>🟥 弓（ 包括击退弓 ）</Text><br></br>
             <Text>🟥 黑曜石</Text><br></br>
-            <Text>🟥 防御塔(速建塔)</Text><br></br>
+            <Text>🟥 防御塔</Text><br></br>
             <Text>🟥 每周轮换道具</Text></p>
         </Secuence>
       </Main>
